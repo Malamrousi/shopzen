b0076300797c8dcc956e1ae41ba90e5208aab73c
DESKTOP-S7FFHIC\h <m.alamrousi.work2@gmail.com>
2025-01-22 10:29:33 +0200

    Add font family arabic and English

---
+++ lib/core/utils/font_family.dart
@@ -0,0 +1,7 @@
+class FontFamily {
+
+  FontFamily._();
+  static const String satoshiEnglish = 'Satoshi';
+  static const String cairoArabic = 'Cairo';
+  //TODO: Switch between arabic and english
+}
