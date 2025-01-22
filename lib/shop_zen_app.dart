ef9364ac541e060a40269f57b4ae712c2e650fd3
DESKTOP-S7FFHIC\h <m.alamrousi.work2@gmail.com>
2025-01-22 09:54:23 +0200

    Add ScreenUtilInit

--- lib/shop_zen_app.dart
+++ lib/shop_zen_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopzen/core/app/connectivity_controller.dart';
 import 'package:shopzen/core/screens/no_network_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ShopZenApp extends StatelessWidget {
   const ShopZenApp({super.key});
@@ -11,26 +12,30 @@ class ShopZenApp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, value, __) {
         if (value) {
-          return MaterialApp(
-            title: 'ShopZen',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            builder: (context, widget) {
-              return Scaffold(
-                body: Builder(
-                  builder: (context) {
-                    ConnectivityController.instance.init();
-                    return widget!;
-                  },
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            child: MaterialApp(
+              title: 'ShopZen',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              builder: (context, widget) {
+                return Scaffold(
+                  body: Builder(
+                    builder: (context) {
+                      ConnectivityController.instance.init();
+                      return widget!;
+                    },
+                  ),
+                );
+              },
+              home: Scaffold(
+                appBar: AppBar(
+                  title: const Text('ShopZen'),
                 ),
-              );
-            },
-            home: Scaffold(
-              appBar: AppBar(
-                title: const Text('ShopZen'),
               ),
             ),
           );
