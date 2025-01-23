afc88adc749cb74804bab222501f5175e45be8a6
DESKTOP-S7FFHIC\h <m.alamrousi.work2@gmail.com>
2025-01-23 13:20:43 +0200

    create dio services

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -2,14 +2,18 @@
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shopzen/core/cubit/app_cubit/app_cubit.dart';
+import 'package:shopzen/core/services/Graphql/api_services.dart';
+import 'package:shopzen/core/services/Graphql/dio_factory.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> setupDependencies() async {
+  final dio = DioFactory.getDio();
   final sharedPreferences = await SharedPreferences.getInstance();
   getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
 
   // App Preferences Instance
 
-  getIt.registerFactory<AppCubit>(() => AppCubit());
+  getIt.registerFactory<AppCubit>(() => AppCubit()); //
+  getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
 }
