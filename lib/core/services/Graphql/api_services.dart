5d3b00744188d012b7ade516122ab6d5dac773ea
DESKTOP-S7FFHIC\h <m.alamrousi.work2@gmail.com>
2025-01-26 11:51:56 +0200

    create upload image

--- lib/core/services/Graphql/api_services.dart
+++ lib/core/services/Graphql/api_services.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:shopzen/core/upload/model/upload_image_response.dart';
 import 'package:shopzen/features/auth/data/models/login_response_model.dart';
 import 'package:shopzen/features/auth/data/models/user_role_model.dart';
 
@@ -10,15 +11,18 @@ const String graphql = '/graphql';
 
 @RestApi(baseUrl: baseUrl)
 abstract class ApiService {
-  factory ApiService(Dio dio, {String baseUrl})= _ApiService;
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 
   @POST(graphql)
   Future<LoginResponseModel> login(
     @Body() Map<String, dynamic> getUser,
   );
 
-    @GET("/api/v1/auth/profile")
-  Future<UserRoleModel> userRole(
-    
+  @GET("/api/v1/auth/profile")
+  Future<UserRoleModel> userRole();
+
+  @POST("/api/v1/files/upload")
+  Future<UploadImageResponse> uploadImage(
+    @Body() FormData? file,
   );
 }
---
+++ lib/core/upload/model/upload_image_response.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'upload_image_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UploadImageResponse _$UploadImageResponseFromJson(Map<String, dynamic> json) =>
+    UploadImageResponse(
+      location: json['location'] as String?,
+    );
+
