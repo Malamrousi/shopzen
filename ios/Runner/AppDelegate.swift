85f5184b77cdb83c55f9c99620bcf65632694b6b
MOHAMMEDALAMROU\LENOVO <m.alamrousi.work2@gmail.com>
2025-04-08 11:08:42 +0200

    Enables Flutter local notifications
    Registers the Flutter local notifications plugin and sets up the user notification center delegate for iOS. This allows the application to send and receive local notifications.
    
    Also, removes an extra character from the main screen view body file.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import GoogleMaps
+import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,12 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+    GeneratedPluginRegistrant.register(with: registry)
+  }
+    if #available(iOS 10.0, *) {
+  UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+}
     GMSServices.provideAPIKey(Environment.)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
--- lib/features/main/presentation/view/widgets/main_screen_view_body.dart
+++ lib/features/main/presentation/view/widgets/main_screen_view_body.dart
@@ -37,4 +37,3 @@ class MainScreenViewBody extends StatelessWidget {
     );
   }
 }
-s
