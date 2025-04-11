e0d9d400e62df029a80f23634a34162a6b99b642
MOHAMMEDALAMROU\LENOVO <m.alamrousi.work2@gmail.com>
2025-04-11 17:18:59 +0200

    Displays dynamic notification count on icon
    Connects the notification icon to a stream of notification data
    and displays the number of unread notifications as a badge on the icon.
    Replaces the static notification count with a dynamic one based on the real-time data.

--- lib/features/main/presentation/view/widgets/notification_bar_icon.dart
+++ lib/features/main/presentation/view/widgets/notification_bar_icon.dart
@@ -4,10 +4,12 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shopzen/core/assets/assets.dart';
 import 'package:shopzen/core/helper/theme_extension.dart';
+import 'package:shopzen/features/notification/data/model/notification_model.dart';
 
 import '../../../../../core/routes/route_name.dart';
 import '../../../../../core/utils/styles/colors_manger.dart';
 import '../../../../../core/utils/styles/test_styles.dart';
+import '../../../../notification/presentation/controller/notification_controller.dart';
 
 class NotificationBarIcon extends StatelessWidget {
   const NotificationBarIcon({super.key});
@@ -18,25 +20,35 @@ class NotificationBarIcon extends StatelessWidget {
       onTap: () {
         Navigator.pushNamed(context, RouteName.notification);
       },
-      child: badges.Badge(
-        showBadge: true,
-        badgeAnimation: const badges.BadgeAnimation.scale(),
-        position: badges.BadgePosition.topEnd(top: -8.h, end: -4.w),
-        badgeContent: Text(
-          "3",
-          style: AppTestStyles.font14Regular(context)
-              .copyWith(fontSize: 12.sp, color: ColorsManger.whiteColor),
-        ),
-        badgeStyle: badges.BadgeStyle(
-            badgeColor: Colors.pink,
-            shape: badges.BadgeShape.circle,
-            padding: REdgeInsets.all(4),
-            elevation: 0),
-        child: SvgPicture.asset(
-          Assets.svgNotificationIcon,
-          color: context.colorThemeExtension.textColor,
-          height: 24.h,
-        ),
+      child: StreamBuilder<List<NotificationModel>>(
+        stream: NotificationController().getNotification(),
+        builder: (context, snapshot) {
+          if(!snapshot.hasData) {
+            return SizedBox.shrink();
+          }else{
+            final numberOfNotification = snapshot.data!.where((e)=>e.isSeen==false).length;
+            return badges.Badge(
+            showBadge: numberOfNotification !=0,
+            badgeAnimation: const badges.BadgeAnimation.scale(),
+            position: badges.BadgePosition.topEnd(top: -8.h, end: -4.w),
+            badgeContent: Text(
+              numberOfNotification >=9 ?"9+":numberOfNotification.toString(),
+              style: AppTestStyles.font14Regular(context)
+                  .copyWith(fontSize: 12.sp, color: ColorsManger.whiteColor),
+            ),
+            badgeStyle: badges.BadgeStyle(
+                badgeColor: Colors.pink,
+                shape: badges.BadgeShape.circle,
+                padding: REdgeInsets.all(4),
+                elevation: 0),
+            child: SvgPicture.asset(
+              Assets.svgNotificationIcon,
+              color: context.colorThemeExtension.textColor,
+              height: 24.h,
+            ),
+          );
+          }
+        }
       ),
     );
   }
